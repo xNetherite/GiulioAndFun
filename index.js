75bcf90aa3f5c6a1c1b44c7eef76532f38dbbdd8
GiulioPaesani <57570629+GiulioPaesani@users.noreply.github.com>
2021-04-23 11:45:03 +0200

    Remove reactions on edited number

--- index.js
+++ index.js
@@ -523,6 +523,8 @@ client.on("messageUpdate", oldMessage => {
                     return
                 }
 
+                oldMessage.reactions.removeAll();
+
                 var titleRandom = ["PENSAVI DI FREGARMI EH!", "CREDI DI FREGARMI?", "TE LO MODIFICHI E IO LO RISCRIVO...", "PENSI DI ESSERE FURBO? BHE LO SEI", "TI SENTI SIMPATICO?"]
                 var embed = new Discord.MessageEmbed()
                     .setTitle(titleRandom[Math.floor(Math.random() * titleRandom.length)])
